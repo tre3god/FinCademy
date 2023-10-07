8602a15c1ea3fb31241266018267e8081dd5b3c4
marcusawd <angweidemarcus@gmail.com>
2023-10-07 22:43:19 +0800

    feat: Create review backend post

---
+++ controllers/api/coursesCtrl.js
@@ -0,0 +1,17 @@
+const Course = require("../../models/Course");
+// const debug = require("debug")("fincademy:controllers:coursesCtrl");
+
+const createReview = async (req, res) => {
+	try {
+		const { courseId } = req.params;
+		const course = await Course.findById(courseId);
+		const newReview = req.body;
+		course.reviews.push(newReview);
+		await course.save();
+		res.status(201).json({ course });
+	} catch (error) {
+		res.status(500).json({ error });
+	}
+};
+
+module.exports = { createReview };

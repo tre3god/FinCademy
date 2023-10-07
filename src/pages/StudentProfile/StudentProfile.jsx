25cc342cac998825a18d458bf9e9e03dd7e5112a
tre3 <goddywin@gmail.com>
2023-10-07 14:46:30 +0800

    mockup of student profile page

--- src/pages/StudentProfile/StudentProfile.jsx
+++ src/pages/StudentProfile/StudentProfile.jsx
@@ -7,6 +7,12 @@ export default function StudentProfile() {
         <>
         <h1>Student Profile Page</h1>
         <h2>Student ID: {userId}</h2>
+        <h3>Below are your enrolled courses</h3>
+        <ul>
+            <li>Finance 101 <button>Unsubscribe</button></li>
+            <li>Money printahhhh <button>Unsubscribe</button></li>
+
+        </ul>
         </>
     )
 }

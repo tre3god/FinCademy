356a3e49ab1946fa3ee5f1add28e164df37014f1
marcusawd <angweidemarcus@gmail.com>
2023-10-09 17:09:34 +0800

    fix: Simon notes

--- src/pages/StudentProfile/StudentProfile.jsx
+++ src/pages/StudentProfile/StudentProfile.jsx
@@ -1,45 +1,46 @@
-import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import * as userService from "../../utilities/users-service";
 
 export default function StudentProfile() {
-    const [studentId, setStudentId] = useState(null)
-    const { userId } = useParams();
-    console.log("from params" + userId)
+	const [studentId, setStudentId] = useState(null);
+	const { userId } = useParams();
+	console.log("from params" + userId);
 
-    useEffect(() => {
-        const fetchStudentDetails = async () => {
-          try {
-            const data = await userService.findStudentCourses(userId);
-            setStudentId(data);
-            
-          } catch (error) { 
-            console.log(error);
-          }
-        };
-        fetchStudentDetails();
-      }, [userId]);
+	useEffect(() => {
+		const fetchStudentDetails = async () => {
+			try {
+				const data = await userService.findStudentCourses(userId);
+				setStudentId(data);
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		fetchStudentDetails();
+	}, [userId]);
 
-    console.log(studentId)
+	console.log(studentId);
 
-    if (!studentId) {
-        return <h1>Loading...</h1>;
-      }
+	if (!studentId) {
+		return <h1>Loading...</h1>;
+	}
 
-    return(
-        <>
-        <h1>Student Profile Page</h1>
-        <h2>Email: {studentId.student ? studentId.student.email : "N/A"}</h2>
-        <h2>Test Courses: {studentId.student ? studentId.student.courses: "N/A"} </h2>
-        <h3>Below are your enrolled courses</h3>
-        
-        {studentId.student.courses.map((course, index) => (
-            <>
-            <div key={index}>
-                <div>{course}</div>
-            </div>
-            </>
-        ))}
-        </>
-    )
+	return (
+		<>
+			<h1>Student Profile Page</h1>
+			<h2>Email: {studentId.student ? studentId.student.email : "N/A"}</h2>
+			<h2>
+				Test Courses: {studentId.student ? studentId.student.courses : "N/A"}{" "}
+			</h2>
+			<h3>Below are your enrolled courses</h3>
+
+			{studentId.student.courses.map((course, index) => (
+				<>
+					<div key={index}>
+						<div>{course}</div>
+					</div>
+				</>
+			))}
+		</>
+	);
 }
--- src/utilities/users-api.js
+++ src/utilities/users-api.js
@@ -2,64 +2,68 @@ const BASE_URL = "/api/users";
 import sendRequest from "./send-request";
 
 const postUserData = async (userData) => {
-  const response = await fetch(BASE_URL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(userData),
-  });
-  if (response.ok) {
-    return await response.json();
-  } else {
-    throw new Error("Invalid Sign up");
-  }
+	const response = await fetch(BASE_URL, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(userData),
+	});
+	if (response.ok) {
+		return await response.json();
+	} else {
+		throw new Error("Invalid Sign up");
+	}
 };
 
 const postUserLogin = async (userData) => {
-  const response = await fetch(`${BASE_URL}/login`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(userData),
-  });
-  if (response.ok) {
-    return await response.json();
-  } else {
-    throw new Error("Invalid Login");
-  }
+	const response = await fetch(`${BASE_URL}/login`, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(userData),
+	});
+	if (response.ok) {
+		return await response.json();
+	} else {
+		throw new Error("Invalid Login");
+	}
 };
 
 const checkToken = async () => {
-  const token = localStorage.getItem("token");
-  const response = await fetch(`${BASE_URL}/check-token`, {
-    method: "GET",
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-  if (response.ok) {
-    return await response.json();
-  } else {
-    throw new Error();
-  }
+	const token = localStorage.getItem("token");
+	const response = await fetch(`${BASE_URL}/check-token`, {
+		method: "GET",
+		headers: {
+			Authorization: `Bearer ${token}`,
+		},
+	});
+	if (response.ok) {
+		return await response.json();
+	} else {
+		throw new Error();
+	}
 };
 
 const findStudentCourses = async (userId) => {
-  const token = localStorage.getItem("token");
+	const token = localStorage.getItem("token");
 
-  const response = await fetch(`${BASE_URL}/${userId}`, {
-    method: "GET",
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-  if (response.ok) {
-    return await response.json();
-  } else {
-    throw new Error();
-  }
+	const response = await fetch(`${BASE_URL}/${userId}`, {
+		method: "GET",
+		headers: {
+			Authorization: `Bearer ${token}`,
+		},
+	});
+	if (response.ok) {
+		return await response.json();
+	} else {
+		throw new Error();
+	}
 };
+
+export function enrollCourse(courseId) {
+	return sendRequest();
+}
 
 export { postUserData, postUserLogin, checkToken, findStudentCourses };

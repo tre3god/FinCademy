bca494ca98dd691feb8da26cccd91368db741e37
marcusawd <angweidemarcus@gmail.com>
2023-10-09 16:03:23 +0800

    refactor: populate review with name

--- src/components/Reviews/ReviewsHistory.jsx
+++ src/components/Reviews/ReviewsHistory.jsx
@@ -6,7 +6,7 @@ const log = debug("fincademy:components:ReviewsHistory");
 export default function ReviewsHistory({ review }) {
 	return (
 		<ListGroup>
-			<ListGroup.Item>{review.user}</ListGroup.Item>
+			<ListGroup.Item>{review.user.name}</ListGroup.Item>
 			<ListGroup.Item>{review.comments}</ListGroup.Item>
 			<ListGroup.Item>
 				<Rating name="rating" value={review.rating} readOnly />

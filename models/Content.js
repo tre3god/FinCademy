5b312ce4a570f07fa330f3d5cde6d7f06b68d7e6
kenjiong <ongshaoqiang@gmail.com>
2023-10-09 09:25:56 +0800

    chore: clean up course content model

--- models/Content.js
+++ models/Content.js
@@ -2,14 +2,25 @@ const mongoose = require("mongoose");
 
 const { Schema, model } = mongoose;
 
-const quizSchema = new Schema({}, { timestamps: true });
+const quizSchema = new Schema(
+  {
+    question: { type: String, required: true },
+    option1: { type: String, required: true },
+    option2: { type: String, required: true },
+    option3: { type: String, required: true },
+    option4: { type: String, required: true },
+    course: { type: Schema.Types.ObjectId, ref: "Course", required: true },
+  },
+  { timestamps: true }
+);
 
 const contentSchema = new Schema(
-	{
-		content: { type: String, required: true },
-		quizzes: [quizSchema],
-	},
-	{ timestamps: true },
+  {
+    content: { type: String, required: true },
+    quiz: [quizSchema],
+    course: { type: Schema.Types.ObjectId, ref: "Course", required: true },
+  },
+  { timestamps: true }
 );
 
 module.exports = model("Content", contentSchema);

9d1c8cf7fc9f47f195822f13aaa884d55b2c71b9
darylcty <124702698+darylcty@users.noreply.github.com>
2023-10-09 22:33:25 +0800

    refact: change href in navbar to onclick to prevent refreshing of page when navigating to new page

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,6 +3,7 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import { logOut } from "../../utilities/users-service";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function NavBar({ user, setUser }) {
 	const handleLogout = () => {
@@ -10,26 +11,31 @@ export default function NavBar({ user, setUser }) {
 		setUser(null);
 	};
 
+	const navigate = useNavigate();
+
 	return (
 		<>
 			{user ? (
 				<Navbar expand="lg" className="bg-body-tertiary">
 					<Container>
-						<img src="https://i.imgur.com/OZTTASE.png" width="50" height="50" />
-						<Navbar.Brand href="/">FinCademy</Navbar.Brand>
+						<div className="logo" onClick={() => navigate(`/`)}>
+							<img src="https://i.imgur.com/OZTTASE.png" width="50" height="50" style={{ marginRight: "20px"}} />
+							<Navbar.Brand>FinCademy</Navbar.Brand>
+						</div>
 						<Navbar.Toggle aria-controls="basic-navbar-nav" />
 						<Navbar.Collapse id="basic-navbar-nav">
 							<Nav className="me-auto">
 								<NavDropdown title="Your Account" id="basic-nav-dropdown">
-									<NavDropdown.Item href={`/users/${user._id}`}>
-										Profile
-									</NavDropdown.Item>
-									<NavDropdown.Divider />
-									<NavDropdown.Item href="/" onClick={handleLogout}>
-										Sign Out
-									</NavDropdown.Item>
+									<NavDropdown.Item onClick={() => navigate(`/users/${user._id}`)}>Profile</NavDropdown.Item>
+								<NavDropdown.Divider />
+										<NavDropdown.Item onClick={() => {
+											navigate("/");
+											handleLogout();
+										}}>
+											Sign Out
+										</NavDropdown.Item>
 								</NavDropdown>
-								<Nav.Link href="/courses">Courses</Nav.Link>
+								<Nav.Link onClick={() => navigate("/courses")}>Courses</Nav.Link>
 							</Nav>
 						</Navbar.Collapse>
 					</Container>
@@ -37,14 +43,16 @@ export default function NavBar({ user, setUser }) {
 			) : (
 				<Navbar expand="lg" className="bg-body-tertiary">
 					<Container>
-						<img src="https://i.imgur.com/OZTTASE.png" width="50" height="50" />
-						<Navbar.Brand href="/">FinCademy</Navbar.Brand>
+						<div className="logo" onClick={() => navigate(`/`)}>
+							<img src="https://i.imgur.com/OZTTASE.png" width="50" height="50" style={{ marginRight: "20px"}} />
+							<Navbar.Brand>FinCademy</Navbar.Brand>
+						</div>
 						<Navbar.Toggle aria-controls="basic-navbar-nav" />
 						<Navbar.Collapse id="basic-navbar-nav">
 							<Nav className="me-auto">
-								<Nav.Link href="/courses">Courses</Nav.Link>
-								<Nav.Link href="signup">Sign Up</Nav.Link>
-								<Nav.Link href="/login">Log In</Nav.Link>
+								<Nav.Link onClick={() => navigate("/courses")}>Courses</Nav.Link>
+								<Nav.Link onClick={() => navigate("/signup")}>Sign Up</Nav.Link>
+								<Nav.Link onClick={() => navigate("/login")}>Log In</Nav.Link>
 							</Nav>
 						</Navbar.Collapse>
 					</Container>

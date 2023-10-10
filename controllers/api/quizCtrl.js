9fdfe8a20faa0c6e4db470911fcbb93dd571ee48
kenjiong <ongshaoqiang@gmail.com>
2023-10-10 15:38:45 +0800

    chore: remove extra code in quizCtrl

--- controllers/api/quizCtrl.js
+++ controllers/api/quizCtrl.js
@@ -1,10 +1,8 @@
-const Course = require("../../models/Course");
 const Quiz = require("../../models/Quiz");
 
 async function getQuiz(req, res) {
 	try {
-		const course = await Course.findById(req.params.courseId); //using courseid to find courseid
-		const quiz = await Quiz.find({ course: course._id });
+		const quiz = await Quiz.find({ course: req.params.courseId });
 		res.status(200).json(quiz);
 	} catch (error) {
 		res.status(500).json({ error });

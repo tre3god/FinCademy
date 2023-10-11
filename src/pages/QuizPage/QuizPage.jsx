a4444eecab273d3b117d10ae729f7335983a4560
kenjiong <ongshaoqiang@gmail.com>
2023-10-11 11:11:22 +0800

    style: slightly change QuizPage styling

--- src/pages/QuizPage/QuizPage.jsx
+++ src/pages/QuizPage/QuizPage.jsx
@@ -5,105 +5,105 @@ import * as quizService from "../../utilities/quiz-service";
 import * as usersService from "../../utilities/users-service";
 
 export default function QuizPage() {
-	const [quiz, setQuiz] = useState([]);
-	const [idx, setIdx] = useState(0);
-	const [score, setScore] = useState(0);
-	const [showScore, setShowScore] = useState(false);
-	const [status, setStatus] = useState("idle");
-	const { courseId } = useParams();
-	const navigate = useNavigate();
+  const [quiz, setQuiz] = useState([]);
+  const [idx, setIdx] = useState(0);
+  const [score, setScore] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+  const [status, setStatus] = useState("idle");
+  const { courseId } = useParams();
+  const navigate = useNavigate();
 
-	useEffect(() => {
-		const fetchQuiz = async () => {
-			try {
-				setStatus("loading");
-				const data = await quizService.getQuiz(courseId);
-				setStatus("success");
-				setQuiz(data);
-			} catch (error) {
-				console.log(error);
-			}
-		};
-		fetchQuiz();
-	}, [courseId]);
+  useEffect(() => {
+    const fetchQuiz = async () => {
+      try {
+        setStatus("loading");
+        const data = await quizService.getQuiz(courseId);
+        setStatus("success");
+        setQuiz(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchQuiz();
+  }, [courseId]);
 
-	const isLoading = status === "loading";
+  const isLoading = status === "loading";
 
-	if (isLoading) {
-		return (
-			<Container className="d-flex justify-content-center vh-100">
-				<Spinner animation="border" variant="success" role="status">
-					<span className="visually-hidden">Loading...</span>
-				</Spinner>
-			</Container>
-		);
-	}
+  if (isLoading) {
+    return (
+      <Container className="d-flex justify-content-center vh-100">
+        <Spinner animation="border" variant="success" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </Spinner>
+      </Container>
+    );
+  }
 
-	// Quiz adapted from https://github.com/chrisblakely01/quiz-app
+  // Quiz adapted from https://github.com/chrisblakely01/quiz-app
 
-	const handleAnswerOptionClick = (index) => {
-		if (index === quiz[idx].isCorrect) {
-			setScore(score + 1);
-		}
+  const handleAnswerClick = (index) => {
+    if (index === quiz[idx].isCorrect) {
+      setScore(score + 1);
+    }
 
-		const nextIdx = idx + 1;
-		if (nextIdx < quiz.length) {
-			setIdx(nextIdx);
-		} else {
-			setShowScore(true);
-		}
-	};
+    const nextIdx = idx + 1;
+    if (nextIdx < quiz.length) {
+      setIdx(nextIdx);
+    } else {
+      setShowScore(true);
+    }
+  };
 
-	const handleExit = async (event) => {
-		event.preventDefault();
-		await usersService.updateQuizScore(score, courseId);
-		navigate("/profile");
-	};
+  const handleExit = async (event) => {
+    event.preventDefault();
+    await usersService.updateQuizScore(score, courseId);
+    navigate("/profile");
+  };
 
   return (
     <div>
       {showScore ? (
         <>
-        <Container className="d-flex justify-content-center">
-          <h3>
-            You scored {score} out of {quiz.length}.
-          </h3>
           <br />
-          <Button size="sm" onClick={handleExit}>
-            Exit Course
-          </Button>
+          <Container className="d-flex justify-content-center">
+            <h4>
+              You scored {score} out of {quiz.length}
+            </h4>
+          </Container>
+          <br />
+          <Container className="d-flex justify-content-center">
+            <Button size="sm" variant="secondary" onClick={handleExit}>
+              Exit Course
+            </Button>
           </Container>
         </>
       ) : (
         <>
-        
-        <Container>
-        <div className="d-flex flex-column align-items-center">
-            <div>
-              <span>Question {idx + 1}</span>/{quiz.length}
+          <br />
+          <Container>
+            <div className="d-flex flex-column align-items-center">
+              <h4>
+                Question {idx + 1} / {quiz.length}
+              </h4>
+              <br />
+              <h5>{quiz[idx]?.question}</h5>
             </div>
             <br />
-            <div>{quiz[idx]?.question}</div>
-          </div>
-          <br />
-          <div className="d-flex flex-column align-items-center">
-            {quiz[idx]?.answers.map((answer, index) => (
-              <>
-                <div className="mb-2">
+            <div className="d-flex flex-column row-gap-1">
+              {quiz[idx]?.answers.map((answer, index) => (
+                <>
                   <Button
                     key={answer._id}
-                    onClick={() => handleAnswerOptionClick(index)}
+                    onClick={() => handleAnswerClick(index)}
                     size="sm"
+                    variant="success"
                   >
                     {answer}
                   </Button>
-                  <br />
-                </div>
-              </>
-            ))}
-          </div>
+                </>
+              ))}
+            </div>
           </Container>
-          
         </>
       )}
     </div>

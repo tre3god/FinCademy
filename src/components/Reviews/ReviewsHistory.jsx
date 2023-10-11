7dacce0b329e7bc4552a5fe2ed8e8b052848cbbd
marcusawd <angweidemarcus@gmail.com>
2023-10-11 16:55:45 +0800

    feat: Review date

--- src/components/Reviews/ReviewsHistory.jsx
+++ src/components/Reviews/ReviewsHistory.jsx
@@ -1,16 +1,23 @@
 import { ListGroup } from "react-bootstrap";
 import { Rating } from "@mui/material";
+import moment from "moment";
 // import debug from "debug";
 // const log = debug("fincademy:components:ReviewsHistory");
 
 export default function ReviewsHistory({ review }) {
+	const isUpdated = review.createdAt !== review.updatedAt;
+	const createdAt = moment(review.createdAt).fromNow();
+	const updatedAt = moment(review.updatedAt).fromNow();
 	return (
 		<ListGroup>
 			<ListGroup.Item>{review.user.name}</ListGroup.Item>
-			<ListGroup.Item>{review.comments}</ListGroup.Item>
-			<ListGroup.Item>
+			<ListGroup.Item className="d-flex">
 				<Rating name="rating" value={review.rating} readOnly />
+				<span className="ms-3">
+					{isUpdated ? `Edited: ${updatedAt}` : `Created: ${createdAt}`}
+				</span>
 			</ListGroup.Item>
+			<ListGroup.Item>{review.comments}</ListGroup.Item>
 		</ListGroup>
 	);
 }

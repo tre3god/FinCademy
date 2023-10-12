5198113f2f2cc51929d702462efda65acbb4a31c
kenjiong <ongshaoqiang@gmail.com>
2023-10-12 10:40:18 +0800

    feat: add backend quiz creation

--- src/utilities/quiz-api.js
+++ src/utilities/quiz-api.js
@@ -4,3 +4,7 @@ const BASE_URL = "/api/quiz"
 export function getQuiz (courseId) {
     return sendRequest(`${BASE_URL}/${courseId}`);
 }
+
+export function create(quiz, courseId) {
+    return sendRequest(`${BASE_URL}/${courseId}`, "POST", quiz);
+}

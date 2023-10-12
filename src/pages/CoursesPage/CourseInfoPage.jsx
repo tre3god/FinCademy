5822cb58db18c43100665736f34c3b2847464cd0
kenjiong <ongshaoqiang@gmail.com>
2023-10-13 00:10:47 +0800

    feat: add error message when already enrolled

--- src/pages/CoursesPage/CourseInfoPage.jsx
+++ src/pages/CoursesPage/CourseInfoPage.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import * as courseService from "../../utilities/course-service";
-import { Button, Col, Row, Container, Spinner } from "react-bootstrap";
+import { Button, Col, Row, Container, Spinner, Alert } from "react-bootstrap";
 import debug from "debug";
 import ReviewsHistory from "../../components/Reviews/ReviewsHistory";
 import { enrollCourse } from "../../utilities/users-service";
@@ -9,95 +9,107 @@ import { enrollCourse } from "../../utilities/users-service";
 const log = debug("fincademy:CoursesPage:CourseInfoPage");
 
 export default function CourseInfoPage({ user, setUser }) {
-	const [course, setCourse] = useState({});
-	const { courseId } = useParams();
-	const [loading, setLoading] = useState(true);
-	const navigate = useNavigate();
+  const [course, setCourse] = useState({});
+  const { courseId } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
 
-	useEffect(() => {
-		const fetchContent = async () => {
-			try {
-				const data = await courseService.getOneCourse(courseId);
-				const { oneCourse } = data;
-				setCourse(oneCourse);
-				setLoading(false);
-				log(oneCourse);
-			} catch (error) {
-				console.log(error);
-				setLoading(false);
-			}
-		};
-		fetchContent();
-	}, [courseId]);
+  useEffect(() => {
+    const fetchContent = async () => {
+      try {
+        const data = await courseService.getOneCourse(courseId);
+        const { oneCourse } = data;
+        setCourse(oneCourse);
+        setLoading(false);
+        log(oneCourse);
+      } catch (error) {
+        console.log(error);
+        setLoading(false);
+      }
+    };
+    fetchContent();
+  }, [courseId]);
 
-	const handleEnroll = async () => {
-		const updatedUser = await enrollCourse({ courseId });
-		setUser(updatedUser);
-		navigate("/profile");
-	};
+  const handleEnroll = async () => {
+    try {
+      const updatedUser = await enrollCourse({ courseId });
+      setUser(updatedUser);
+      navigate("/profile");
+    } catch (error) {
+      setError("You are already enrolled in this course.");
+    }
+  };
 
-	if (loading) {
-		return (
-			<Container className="d-flex justify-content-center vh-100">
-				<Spinner animation="border" variant="success" role="status">
-					<span className="visually-hidden">Loading...</span>
-				</Spinner>
-			</Container>
-		);
-	}
+  if (loading) {
+    return (
+      <Container className="d-flex justify-content-center vh-100">
+        <Spinner animation="border" variant="success" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </Spinner>
+      </Container>
+    );
+  }
 
-	return (
-		<Container>
-			<Row>
-				<Col>
-					<img
-						src={course?.img}
-						style={{ width: "100%", height: "auto", maxHeight: "450px" }}
-					/>
-				</Col>
-			</Row>
-			<Row>
-				<Col>
-					<h1>{course?.courseTitle}</h1>
-				</Col>
-			</Row>
-			<Row>
-				<Col>
-					<h2>Description</h2>
-					<div className="p-2">{course?.longDescription}</div>
-				</Col>
-			</Row>
-			<Row>
-				<Col>
-					<h3>Price</h3>
-					<div className="p-2">Course Price: ${course?.price}</div>
-				</Col>
-			</Row>
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <img
+            src={course?.img}
+            style={{ width: "100%", height: "auto", maxHeight: "450px" }}
+          />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <h1>{course?.courseTitle}</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <h2>Description</h2>
+          <div className="p-2">{course?.longDescription}</div>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <h3>Price</h3>
+          <div className="p-2">Course Price: ${course?.price}</div>
+        </Col>
+      </Row>
 
-			{user ? (
-				<Row className="justify-content-center">
-					<Button variant="success" className="col-md-5" onClick={handleEnroll}>
-						Enroll
-					</Button>
-				</Row>
-			) : (
-				<Row className="justify-content-center">
-					<Button
-						variant="warning"
-						className="col-md-5"
-						onClick={() => navigate("/login")}>
-						Login to Enroll
-					</Button>
-				</Row>
-			)}
-			<Row>
-				<Col>
-					<h3>Reviews</h3>
-					{course?.reviews?.map((review) => (
-						<ReviewsHistory key={review._id} review={review} />
-					))}
-				</Col>
-			</Row>
-		</Container>
-	);
+      {user ? (
+        <Row className="justify-content-center">
+          <Button variant="success" className="col-md-5" onClick={handleEnroll}>
+            Enroll
+          </Button>
+        </Row>
+      ) : (
+        <Row className="justify-content-center">
+          <Button
+            variant="warning"
+            className="col-md-5"
+            onClick={() => navigate("/login")}
+          >
+            Login to Enroll
+          </Button>
+        </Row>
+      )}
+      <br />
+      {error && (
+        <Alert variant="danger" className="text-center">
+          {error}
+        </Alert>
+      )}
+      <Row>
+        <Col>
+          <h3>Reviews</h3>
+          {course?.reviews?.map((review) => (
+            <ReviewsHistory key={review._id} review={review} />
+          ))}
+        </Col>
+      </Row>
+    </Container>
+  );
 }
